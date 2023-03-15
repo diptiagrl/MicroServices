0220bd629629815736168705d3fab706b65f64a8
Shalini Mittal <shalini06.mittal@gmail.com>
2023-03-15 13:12:39 +0530

    day 3 ms

--- ConfigServerDemo/src/main/java/com/citi/ms/ConfigServerDemoApplication.java
+++ ConfigServerDemo/src/main/java/com/citi/ms/ConfigServerDemoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerDemoApplication {
-
+	//http://localhost:8888/config-server-client/development
+	//http://localhost:8888/config-server-client/default
+	//http://localhost:8888/config-server-client/production
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerDemoApplication.class, args);
 	}
