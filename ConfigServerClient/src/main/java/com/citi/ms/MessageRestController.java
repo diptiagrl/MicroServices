8dd32edcc0b10c0c55727445d3be09ff5ebaa734
Shalini Mittal <shalini06.mittal@gmail.com>
2023-03-15 13:20:58 +0530

    day 3 ms

--- ConfigServerClient/src/main/java/com/citi/ms/MessageRestController.java
+++ ConfigServerClient/src/main/java/com/citi/ms/MessageRestController.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//http://localhost:8081/actuator/refresh - POST
+
 @RefreshScope
 @RestController
 
